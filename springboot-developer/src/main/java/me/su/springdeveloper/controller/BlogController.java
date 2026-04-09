6fbb37147764887bab295c9313ec2caa9442bf90
su <ysm4885@naver.com>
2026-04-09 14:19:33 +0900

    Ch06

--- springboot-developer/src/main/java/me/su/springdeveloper/controller/BlogController.java
+++ springboot-developer/src/main/java/me/su/springdeveloper/controller/BlogController.java
@@ -3,14 +3,13 @@ package me.su.springdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.su.springdeveloper.dao.Article;
 import me.su.springdeveloper.dto.AddArticleRequest;
+import me.su.springdeveloper.dto.ArticleResponse;
 import me.su.springdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 
 @RestController
@@ -20,10 +19,15 @@ public class BlogController {
 
     @PostMapping("/api/articles")
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest addArticleRequest) {
-        // 수정: 변수명을 addArticleRequest로 일치시킴
         Article savedArticle = blogService.save(addArticleRequest);
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> result = blogService.findAll()
+                .stream().map(ArticleResponse::new).toList();
+        return ResponseEntity.ok().body(result);
     }
 }
--- springboot-developer/src/main/java/me/su/springdeveloper/service/BlogService.java
+++ springboot-developer/src/main/java/me/su/springdeveloper/service/BlogService.java
@@ -7,6 +7,8 @@ import me.su.springdeveloper.dto.AddArticleRequest;
 import me.su.springdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class BlogService {
@@ -15,7 +17,11 @@ public class BlogService {
     // DTO를 매개변수로 받도록 수정
     public Article save(AddArticleRequest request){
         // request(DTO)를 엔티티로 변환하여 저장
-        // 보통 AddArticleRequest 클래스 안에 toEntity() 메서드를 만들어 사용합니다.
+        // 보통 AddArticleRequest 클래스 안에 toEntity() 메서드를 만들어 사용
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll(){
+        return blogRepository.findAll();
     }
 }
